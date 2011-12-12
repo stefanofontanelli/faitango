8d08f66d6951b99bc1c56ffddb00b8db0305bb05
Christian Nastasi <nastasichr@gmail.com>
2011-12-12 16:45:21 +0100

    DataEventParser Factory
    Add a Factory class to create DataEventParser products (as for now we only have
    the JSON one). The static initialization (in factory registry) and the java
    reflection have been adopted to implement the factory method pattern.

---
+++ FAItango/src/com/retis/faitango/DataEvent.java
@@ -0,0 +1,12 @@
+package com.retis.faitango;
+
+public final class DataEvent {
+	
+	// chris NOTE:Fields taken from the JSON file they gave us
+	public String id;
+	public String text;
+	public String date;
+	public String city;
+	public String type;
+	public String af; // chris TODO: what is this? give a better name
+}
