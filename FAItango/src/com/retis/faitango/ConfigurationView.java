b6bc14042378c1481cee8ba6f226242f9cc07194
Christian Nastasi <nastasichr@gmail.com>
2012-01-17 17:58:32 +0100

    Change Preference Menu
    The preference menu has been changed according to the specification defined by
    the UI mock-ups. Consequently, strings have been added and the
    ConfigurationView activity has been adapted as well (callbacks).

--- FAItango/src/com/retis/faitango/ConfigurationView.java
+++ FAItango/src/com/retis/faitango/ConfigurationView.java
@@ -1,7 +1,7 @@
 package com.retis.faitango;
 
+import com.retis.faitango.R;
 import android.os.Bundle;
-import android.preference.CheckBoxPreference;
 import android.preference.ListPreference;
 import android.preference.Preference;
 import android.preference.PreferenceActivity;
@@ -9,58 +9,94 @@ import android.widget.Toast;
 
 public class ConfigurationView extends PreferenceActivity {
 
-	private CheckBoxPreference bgUpdaterCheckbox;
-	private ListPreference bgUpdaterPeriodsList;
-	private boolean bgUpdaterOn;
+	private ListPreference autoSyncType;
+	private ListPreference periodicAutoSyncPeriods;
+	private boolean periodicAutoSync;
 	
 	 @Override
 	 protected void onCreate(Bundle savedInstanceState) {
 		 super.onCreate(savedInstanceState);
 	     // Load the preferences from an XML resource
 		 addPreferencesFromResource(R.xml.preferences);
-		 createBgUpdaterLayout();
+		 configureAutoSyncType();
+		 configurePeriodicAutoSyncPeriods();
+		 configureSearchParams();
 	 }
 	    
     /*****************************************/
-    /*** Background Updater configuration ***/
+    /***Cccdslkdjsalkdjaslkd configuration ***/
     /*****************************************/    
-    private void createBgUpdaterLayout() {
+    private void configureAutoSyncType() {
     	
-    	// CheckBox setup
-        bgUpdaterCheckbox = (CheckBoxPreference) findPreference("checkboxBgUpdater");
-        bgUpdaterOn = bgUpdaterCheckbox.isChecked();
-        bgUpdaterCheckbox.setOnPreferenceChangeListener(new Preference.OnPreferenceChangeListener() {
+    	// List setup
+        autoSyncType = (ListPreference) findPreference("autoSyncType");  
+        autoSyncType.setSummary(autoSyncType.getEntry());
+        autoSyncType.setOnPreferenceChangeListener(new Preference.OnPreferenceChangeListener() {
         	public boolean onPreferenceChange(Preference pce, Object value) {
-        		EventReaderAlarm alarm = new EventReaderAlarm(pce.getContext());
-            	String s;
-            	bgUpdaterOn = value.equals(true); 
-            	if (bgUpdaterOn) {
-               		alarm.start();
-               		s = "Starting background updater";
-            	} else {
-            		alarm.stop();
-            		s = "Stopping background updater";
-            	}
-        		Toast.makeText(pce.getContext(), s, Toast.LENGTH_SHORT).show();
+        		long type = Long.decode((String)value);
+        		if (type == 0) {
+        			periodicAutoSync = false;
+        		} else if (type == 1) {
+        			periodicAutoSync = false;
+        		} else if (type == 2) {
+        			periodicAutoSync = true;
+        		} else {
+        			// TODO: generate some error?
+        		}
+        		int idx = autoSyncType.findIndexOfValue((String)value);
+        		pce.setSummary(autoSyncType.getEntries()[idx]);
         		return true;
         	}
         });
+    }
+    
+    private void configurePeriodicAutoSyncPeriods() {
 
         // List setup
-        bgUpdaterPeriodsList = (ListPreference) findPreference("listBgUpdaterPeriods");  
-        bgUpdaterPeriodsList.setOnPreferenceChangeListener(new Preference.OnPreferenceChangeListener() {
+        periodicAutoSyncPeriods = (ListPreference) findPreference("periodicAutoSyncPeriods");  
+        periodicAutoSyncPeriods.setSummary(periodicAutoSyncPeriods.getEntry());
+        periodicAutoSyncPeriods.setOnPreferenceChangeListener(new Preference.OnPreferenceChangeListener() {
         	public boolean onPreferenceChange(Preference pce, Object value) {
-        		if (bgUpdaterOn) {
+        		if (periodicAutoSync) {
         			// Update alarm if necessary
         			long period =  Long.decode((String)value);
-                	period = period * 1000; // In milliseconds
-        			EventReaderAlarm alarm = new EventReaderAlarm(pce.getContext(), period, period);
-        			alarm.start();
+        			if (period != 0) {
+        				period = period * 1000; // In milliseconds
+        				EventReaderAlarm alarm = new EventReaderAlarm(pce.getContext(), period, period);
+        				alarm.start();
+        			}
         		}
             	String s = "UpdaterPerdiods = " + (String)value;
         		Toast.makeText(pce.getContext(), s, Toast.LENGTH_SHORT).show();
+        		int idx = periodicAutoSyncPeriods.findIndexOfValue((String)value);
+        		pce.setSummary(periodicAutoSyncPeriods.getEntries()[idx]);
         		return true;
         	}
         });    
     }
+    
+    private void configureSearchParams() {
+        
+    	// FIXME: TEMPORARY SOLUTION: treat each change as the same
+    	// TODO:  The Country, Region, Province preference should be influenced each other according
+    	//        to the following hierarchical order: Contry->Region->Province!!!
+        ListPreference country = (ListPreference) findPreference("autoSyncSearchCountry");  
+        ListPreference region = (ListPreference) findPreference("autoSyncSearchRegion");
+        ListPreference province = (ListPreference) findPreference("autoSyncSearchProvince");
+        addSearchPreference(country);
+        addSearchPreference(region);
+        addSearchPreference(province);
+    }
+    
+    private void addSearchPreference(ListPreference pref) {
+    	pref.setSummary(pref.getEntry());
+    	pref.setOnPreferenceChangeListener(new Preference.OnPreferenceChangeListener() {
+        	public boolean onPreferenceChange(Preference pce, Object value) {
+        		ListPreference lp = (ListPreference) pce;
+        		int idx = lp.findIndexOfValue((String)value);
+        		pce.setSummary(lp.getEntries()[idx]);
+        		return true;
+        	}
+        });
+    }    
 }
